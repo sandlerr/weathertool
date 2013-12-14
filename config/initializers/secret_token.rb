113cb38dfb5dd081e62ef58ac2986d1c19cc0f9b
Roman Sandler <rsandler@zendesk.com>
2013-12-14 13:55:30 +1100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Weathertool::Application.config.secret_key_base = '941423df32068fe3440aa9367b6b174cb13531bb2ec7ae0114e2d86ca107c84082bad813ea4018413f1f770fa039b2f8fcbacefb87c5b46cf33f0fa3b801c6a1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Weathertool::Application.config.session_store :cookie_store, key: '_weathertool_session'
